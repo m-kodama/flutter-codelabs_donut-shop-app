ef7ed4ad6b87cf7d705ad523fa97b920ae031204
m-kodama <tc003ssr@gmail.com>
2022-04-17 20:17:03 +0900

    feat: スプラッシュページを作成

---
+++ lib/ui/core/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color mainColor = Color(0xFFFF0F7E);
+  static const Color mainDark = Color(0xFF980346);
+}
---
+++ lib/ui/splash/page/splash_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_codelab_donut_shop_app/ui/core/app_colors.dart';
+import 'package:flutter_codelab_donut_shop_app/ui/core/image_urls.dart';
+import 'package:flutter_codelab_donut_shop_app/ui/donut_shop_main/page/donut_shop_main.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController? donutController;
+  Animation<double>? rotateAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    donutController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )..repeat();
+
+    rotateAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: donutController!,
+        curve: Curves.linear,
+      ),
+    );
+
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => const DonutShopMain(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.mainColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            RotationTransition(
+              turns: rotateAnimation!,
+              child: Image.network(
+                ImageUrls.donutLogoWhiteNoText,
+                width: 100,
+                height: 100,
+              ),
+            ),
+            Image.network(
+              ImageUrls.donutLogoWhiteText,
+              width: 150,
+              height: 150,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    donutController?.dispose();
+    super.dispose();
+  }
+}
